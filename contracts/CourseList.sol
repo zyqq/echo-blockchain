b75dc84514ac37c039fbd88944d580508ff05ec0
zyqq <252545095@qq.com>
2019-03-20 16:36:01 +0800

    feat: 手动编译合约完成

---
+++ contracts/CourseList.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+contract CourseList{
+  address public ceo;
+
+  constructor() public {
+    ceo = msg.sender;
+  }
+}
---
+++ scripts/compile.js
@@ -0,0 +1,34 @@
+// 文件模块
+const fs = require('fs')
+const path = require('path')
+const solc = require('solc')
+
+// 合约路径
+const contractPath = path.resolve(__dirname, '../contracts/CourseList.sol')
+// 获取合约文件内容
+const contractSource = fs.readFileSync(contractPath, 'utf-8')
+
+//预先定义好编译源json对象
+let jsonContractSource = JSON.stringify({
+  language: 'Solidity',
+  sources: {
+    'CourseList': {  // 指明编译的文件名
+      content: contractSource, // solidity 源代码
+    },
+  },
+  settings: { // 自定义编译输出的格式。以下选择输出全部结果。
+      outputSelection: {
+    '*': {
+      '*': [ '*' ]
+    }
+  }
+  },
+})
+// 编译
+let output = JSON.parse(solc.compile(jsonContractSource));  
+console.log(output)
+Object.keys(output.contracts).forEach(name => {
+  const filePath = path.resolve(__dirname, `../src/compiled/${name}.sol`)
+  fs.writeFileSync(filePath, JSON.stringify(output.contracts[name]))
+  console.log(`${filePath} bingo`)
+})
