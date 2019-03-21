b9b193a587737439bf1f608ac025c78d1da2004a
zyqq <252545095@qq.com>
2019-03-21 09:01:45 +0800

    fix: 合约部署

--- test/course.spec.js
+++ test/course.spec.js
@@ -5,7 +5,8 @@ const Web3 = require('web3')
 // 本地虚拟的以太坊环境
 const ganache = require('ganache-cli')
 // 创建web3实例链接私有链
-const web3 = new Web3(ganache.provider())
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// const web3 = new Web3(ganache.provider())
 // 引入合约
 const CourseList = require(path.resolve(__dirname, '../src/compiled/CourseList.json'))
 const Course = require(path.resolve(__dirname, '../src/compiled/Course.json'))
@@ -21,15 +22,18 @@ describe('测试课程的智能合约', () => {
     // 1. 虚拟部署一个合约
     console.log(accounts)
     console.log('账户余额：' + await web3.eth.getBalance(accounts[9]))
-
+    
     courseList = await new web3.eth.Contract(CourseList.abi)
-            .deploy({data: CourseList.evm.bytecode.object})
+            .deploy({
+              data: '0x' + CourseList.evm.bytecode.object, // 需要注意 字节码需要添加 '0x' 不然会有各种错误   
+              arguments: 'CourseList'
+            })
             .send({
               // 最后一个是部署者
               from: accounts[9],
               gas: '5000000'
             })
-            console.log(courseList)
+    console.log(courseList)
   })
   // it('测试添加课程',async () => {
   //   assert.equal(1+1, 2)
